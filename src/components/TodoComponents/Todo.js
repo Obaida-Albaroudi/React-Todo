2ae2c93fadd4cf1b5053f1c7fb0389ec6b2e879c
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-08-05 12:31:37 -0700

    Added the fucntions and necessary scaffolding.

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todo = props =>{
+    return (
+        <div onClick={() => props.toggleTodo(props.todo.id)}>
+
+            <p>{props.todo.task}</p>
+            
+        </div>
+    )
+}
